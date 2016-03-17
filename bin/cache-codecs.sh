ecd7b5b20961e87b8a2d8ff56f7aba5ff8d44f52
nayrnet <admin@nayr.net>
2016-03-17 15:05:22 -0600

    added codec detection

--- bin/cache-codecs.sh
+++ bin/cache-codecs.sh
@@ -5,7 +5,7 @@
 FILE='/tmp/codec-support.json'
 rm $FILE
 for codec in H264 MPG2 WVC1 MPG4 MJPG WMV9; do
-        LINE=`echo -e "$(vcgencmd codec_enabled $codec)"|sed 's/=/":"/g'`
-        echo $LINE >> $FILE
+	LINE=`echo -e "$(vcgencmd codec_enabled $codec)"|sed 's/=/":"/g'`
+	echo $LINE >> $FILE
 done
-sed -e '1i {' -e 's/./    "&/; s/$/",/; $s/,$//; $a }' $FILE
+sed -i -e '1i {' -e 's/./    "&/; s/$/",/; $s/,$//; $a }' $FILE
